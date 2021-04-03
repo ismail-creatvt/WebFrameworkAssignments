e6c42fef612255ad3dc31f38dccd1f57ccb8c3e1
Ismail Shaikh <ismailshaikh31097@gmail.com>
2021-04-04 02:03:00 +0530

    Question 8 completed

---
+++ question8/index.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql')
+const con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Admin@9615'
+})
+
+con.connect((err)=>{
+    if(err) {
+        console.log(err)
+        return
+    }
+    con.query("DROP DATABASE QUESTION8")
+    con.query("CREATE DATABASE QUESTION8")
+    con.query("USE QUESTION8")
+    con.query("CREATE TABLE Student(id integer primary key, name varchar(50))")
+    console.log("Database created!!")
+})
