d751c7c874d5d8745485365d00bfabf93d2c4692
Ismail Shaikh <ismailshaikh31097@gmail.com>
2021-04-04 00:58:22 +0530

    Question 6 completed

---
+++ question6/index.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    const resourceArray = req.url.split("/")
+    const name = resourceArray[resourceArray.length - 1]
+    const resource = fs.createReadStream(name)
+
+    resource.on('open', () => {
+        res.writeHead(200, {'Content-dispositon':`attachment:${name}`})
+        const result = resource.pipe(res)
+        result.on('finish', () => {
+            res.end()
+        })
+    })
+
+    resource.on('error', (err)=>{
+        res.writeHead(404)
+        res.end()
+    })
+})
+
+server.listen(8888, () => {
+    console.log(`server is listening on port 8888`)
+})
