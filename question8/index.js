8b9357e3f2cdd1f16ce6c608132332586c422638
Ismail Shaikh <ismailshaikh31097@gmail.com>
2021-04-04 02:15:39 +0530

    Question 9 Completed

--- question8/index.js
+++ question8/index.js
@@ -10,7 +10,7 @@ con.connect((err)=>{
         console.log(err)
         return
     }
-    con.query("DROP DATABASE QUESTION8")
+    con.query("DROP DATABASE IF EXISTS QUESTION8")
     con.query("CREATE DATABASE QUESTION8")
     con.query("USE QUESTION8")
     con.query("CREATE TABLE Student(id integer primary key, name varchar(50))")
---
+++ question9/index.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql')
+
+const con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Admin@9615'
+})
+
+con.connect((err)=>{
+    if(err){
+        console.log(err)
+        return
+    }
+
+    con.query("DROP DATABASE IF EXISTS QUESTION9")
+    con.query("CREATE DATABASE QUESTION9")
+    con.query("USE QUESTION9")
+    con.query("CREATE TABLE CUSTOMER(id integer primary key, name varchar(80), address varchar(100))")
+
+    const insertQuery = "INSERT INTO CUSTOMER(id, name, address) VALUES (1, 'Ismail', 'Gagan Apartment, Flat no 5., Bhairav Nagar, Pune -15'), (2, 'Arbaaz', 'S/230, Keshav Nagari, Viman Nagar, Pune -15'), (3, 'Asif', '438, Fashion Street, Camp, Pune -1')"
+    con.query(insertQuery, (err, result)=>{
+        if(err){
+            console.log(err)
+            return
+        }
+        console.log(result)
+    })
+})
